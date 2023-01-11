55d95415a00b95b8d8e56431415f63dde2bda5f7
drealdan1 <drealdan1@gmail.com>
2023-01-10 17:54:12 -0800

    swap numbers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,21 +1,19 @@
 #include "main.h"
 /**
- *
- *swap_int - To swap two values a and b in memory
- *
- *
- *@a: first value in memory
- *@b: Second value in memory
- *
- * 
- */
-void swap_int(int *a, int *b){
+* swap_int - To swap two values a and b in memory
+*
+* @a: first value in memory
+* @b: Second value in memory
+*
+*
+*/
+void swap_int(int *a, int *b)
+{
 
 	int c = *a;
 
 	*a = *b;
 	*b = c;
-
 
 }
 
