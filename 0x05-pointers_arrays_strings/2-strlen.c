9f37bcf02dd3316b9d890a412c86069d73d280a0
drealdan1 <drealdan1@gmail.com>
2023-01-10 18:16:06 -0800

    string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * _strlen - evaluates the length of a string
+ * @s: the string
+ *
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		;
+	return (i);
+
+
+
+}
